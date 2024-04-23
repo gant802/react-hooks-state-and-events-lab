6d8005145c367f22b55be8eda99f10cbc3f4a9de
Grant Cummings <grantcummings4429@gmail.com>
2024-04-23 09:12:08 -0400

    finished lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+const [isOn, setIsOn] = useState(false);
+
+function handleClick() {
+  setIsOn(isOn => !isOn)
+}
+
+const appClass = isOn ? "in-cart" : ""
+const buttonClass = isOn ? "remove" : "add"
+
   return (
-    <li className="">
+    <li className={appClass} onClick={handleClick}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass}>{isOn ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
